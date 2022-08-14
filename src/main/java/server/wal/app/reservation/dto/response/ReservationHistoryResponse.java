823810eedfad8373a88522dc5f055a74f4972875
chanwoo <chocw0402@gmail.com>
2022-08-14 21:52:10 +0900

    [feat] 왈소리 히스토리 response

---
+++ src/main/java/server/wal/app/reservation/dto/response/ReservationHistoryResponse.java
@@ -0,0 +1,29 @@
+package server.wal.app.reservation.dto.response;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ReservationHistoryResponse {
+
+    private List<ReservationResponseDto> incompleteData;
+    private List<ReservationResponseDto> completeData;
+
+    @Builder(access = AccessLevel.PACKAGE)
+    public ReservationHistoryResponse(List<ReservationResponseDto> incompleteData, List<ReservationResponseDto> completeData) {
+        this.incompleteData = incompleteData;
+        this.completeData = completeData;
+    }
+
+    public static ReservationHistoryResponse of(List<ReservationResponseDto> notDoneReservation, List<ReservationResponseDto> doneReservation) {
+        return ReservationHistoryResponse.builder()
+                .incompleteData(notDoneReservation)
+                .completeData(doneReservation)
+                .build();
+    }
+
+}
