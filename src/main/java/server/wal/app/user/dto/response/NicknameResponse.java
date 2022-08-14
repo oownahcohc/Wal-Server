db260da0355319eec771c2f6b7760c92193b35c5
chanwoo <chocw0402@gmail.com>
2022-08-14 21:48:28 +0900

    [feat] 닉네임 변경 response

---
+++ src/main/java/server/wal/app/user/dto/response/NicknameResponse.java
@@ -0,0 +1,22 @@
+package server.wal.app.user.dto.response;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+public class NicknameResponse {
+
+    private String nickname;
+
+    private NicknameResponse() {}
+
+    private NicknameResponse(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public static NicknameResponse of(String nickname) {
+        return new NicknameResponse(nickname);
+    }
+
+}
