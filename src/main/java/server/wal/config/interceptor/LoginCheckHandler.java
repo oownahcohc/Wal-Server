b3931d388c6c5b410c81756328f635089f8e05a4
chanwoo <chocw0402@gmail.com>
2022-08-14 21:27:21 +0900

    [feat] login user 확인 클래스

---
+++ src/main/java/server/wal/config/interceptor/LoginCheckHandler.java
@@ -0,0 +1,32 @@
+package server.wal.config.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import server.wal.common.exception.custom.UnAuthorizedException;
+import server.wal.common.util.HttpHeaderUtils;
+import server.wal.common.util.JwtUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@RequiredArgsConstructor
+public class LoginCheckHandler {
+
+    private final JwtUtils jwtProvider;
+
+    public Long getUserId(HttpServletRequest request) {
+        String bearerToken = request.getHeader(HttpHeaderUtils.AUTH_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(HttpHeaderUtils.BEARER_TOKEN)) {
+            String accessToken = bearerToken.substring(HttpHeaderUtils.BEARER_TOKEN.length());
+            if (jwtProvider.validateToken(accessToken)) {
+                Long userId = jwtProvider.getUserIdFromJwt(accessToken);
+                if (userId != null) {
+                    return userId;
+                }
+            }
+        }
+        throw new UnAuthorizedException(String.format("잘못된 JWT (%s) 입니다", bearerToken));
+    }
+
+}
