5665eb3e1fdee6c174aa512419ed8efbdc9cc554
chanwoo <chocw0402@gmail.com>
2022-08-14 21:29:46 +0900

    [feat] jwt를 통한 유저 구분 constants

---
+++ src/main/java/server/wal/config/security/JwtConstants.java
@@ -0,0 +1,11 @@
+package server.wal.config.security;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class JwtConstants {
+
+    public static final String USER_ID = "USER_ID";
+
+}
