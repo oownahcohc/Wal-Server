672fd3cafe5c840fde781a972fa1eea579c13305
chanwoo <chocw0402@gmail.com>
2022-08-14 21:53:04 +0900

    [feat] reservationId response

---
+++ src/main/java/server/wal/app/reservation/dto/response/ReservationIdResponse.java
@@ -0,0 +1,22 @@
+package server.wal.app.reservation.dto.response;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+public class ReservationIdResponse {
+
+    private Long reservationId;
+
+    private ReservationIdResponse() {}
+
+    private ReservationIdResponse(Long reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public static ReservationIdResponse from(Long reservationId) {
+        return new ReservationIdResponse(reservationId);
+    }
+
+}
