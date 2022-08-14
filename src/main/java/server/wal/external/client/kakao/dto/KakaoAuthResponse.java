4b41a27dbe84c3811da138af5ef34acd14dbcf9c
chanwoo <chocw0402@gmail.com>
2022-08-14 21:15:06 +0900

    [feat] kakao 유저 정보 response dto

---
+++ src/main/java/server/wal/external/client/kakao/dto/KakaoAuthResponse.java
@@ -0,0 +1,25 @@
+package server.wal.external.client.kakao.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class KakaoAuthResponse {
+
+    private String id;
+
+    private KakaoAuthResponse(String id) {
+        this.id = id;
+    }
+
+}
+
