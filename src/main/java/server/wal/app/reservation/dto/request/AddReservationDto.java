cd2545ed34d5591096e07447e6953e8f58556c99
chanwoo <chocw0402@gmail.com>
2022-08-14 21:50:21 +0900

    [feat] 왈소리 예약 request dto

---
+++ src/main/java/server/wal/app/reservation/dto/request/AddReservationDto.java
@@ -0,0 +1,26 @@
+package server.wal.app.reservation.dto.request;
+
+import lombok.*;
+import server.wal.common.util.TimeUtils;
+import server.wal.domain.common.enumerate.ShowStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class AddReservationDto {
+
+    private String contents;
+    private LocalDate localDate;
+    private LocalTime localTime;
+    private ShowStatus showStatus;
+
+    public static AddReservationDto of(String contents, String localDate, String localTime, ShowStatus showStatus) {
+        return new AddReservationDto(contents, LocalDate.parse(localDate), LocalTime.parse(localTime), showStatus);
+    }
+
+}
