29b8bf8103e6bf75b0d144eaaf8da24c85d4e636
chanwoo <chocw0402@gmail.com>
2022-08-15 16:00:11 +0900

    [add] EnableScheduling 추가

--- src/main/java/server/wal/WalApplication.java
+++ src/main/java/server/wal/WalApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableScheduling
 @EnableFeignClients
 @EnableJpaAuditing
 @EnableSwagger2
