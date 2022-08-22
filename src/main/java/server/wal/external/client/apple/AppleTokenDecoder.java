bc9e5cf59a9fd387a40d86eaf57a88a3a94d2df4
chanwoo <chocw0402@gmail.com>
2022-08-22 18:52:09 +0900

    [style] 줄바꿈

--- src/main/java/server/wal/external/client/apple/AppleTokenDecoder.java
+++ src/main/java/server/wal/external/client/apple/AppleTokenDecoder.java
@@ -3,7 +3,5 @@ package server.wal.external.client.apple;
 import org.jetbrains.annotations.NotNull;
 
 public interface AppleTokenDecoder {
-
     String getSocialIdFromIdToken(@NotNull String idToken);
-
 }
