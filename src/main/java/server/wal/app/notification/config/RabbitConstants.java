ed227ee1cd2176e4558bdded425cc1961e1f172b
chanwoo <chocw0402@gmail.com>
2022-08-25 02:13:50 +0900

    [add] firebase-admin

--- src/main/java/server/wal/app/notification/config/RabbitConstants.java
+++ src/main/java/server/wal/app/notification/config/RabbitConstants.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
+//@PropertySource(value = "classpath:application-rabbit.yml", factory = YamlPropertySourceFactory.class, ignoreResourceNotFound = true)
 public final class RabbitConstants {
 
     public static final String MORNING_QUEUE = "MORNING_QUEUE";
@@ -12,5 +13,10 @@ public final class RabbitConstants {
     public static final String RESERVATION_QUEUE = "RESERVATION_QUEUE";
 
     public static final String EXCHANGE_NAME = "TIME.TOPIC";
+
+    public static final String MORNING_ROUTING_KEY = "MORNING";
+    public static final String AFTERNOON_ROUTING_KEY = "AFTERNOON";
+    public static final String NIGHT_ROUTING_KEY = "NIGHT";
+    public static final String RESERVATION_ROUTING_KEY = "RESERVATION";
 
 }
