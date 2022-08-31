62dfa8e7f199a02d3b331e5e454a26e9ecec1f2f
chanwoo <chocw0402@gmail.com>
2022-08-31 18:26:09 +0900

    [feat] fcm message constants 형식 class

---
+++ src/main/java/server/wal/app/notification/firebase/constants/WalFCMConstants.java
@@ -0,0 +1,13 @@
+package server.wal.app.notification.firebase.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class WalFCMConstants {
+
+    public static final String TITLE_KEY = "title";
+    public static final String TITLE_VALUE = "오늘의 왈소리가 도착했어요!";
+    public static final String CONTENT_KEY = "content";
+
+}
