3520e347241e8f6831cf8f3d328955cdb65ffb38
chanwoo <chocw0402@gmail.com>
2022-08-31 18:28:11 +0900

    [feat] wal consumer

--- src/main/java/server/wal/app/notification/activemq/consumer/WalConsumer.java
+++ src/main/java/server/wal/app/notification/activemq/consumer/WalConsumer.java
@@ -1,4 +1,21 @@
 package server.wal.app.notification.activemq.consumer;
 
-public class RabbitConsumer {
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import server.wal.app.notification.activemq.constant.ActiveMQConstants;
+import server.wal.app.notification.firebase.FCMService;
+
+@Component
+@RequiredArgsConstructor
+public class WalConsumer {
+
+    private final FCMService fcmService;
+
+    @JmsListener(destination = ActiveMQConstants.RESERVATION_QUEUE)
+    public void reservationConsumer(@Payload Long userId) {
+        fcmService.sendReservationWal(userId);
+    }
+
 }
