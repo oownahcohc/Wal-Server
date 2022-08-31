7d6304371c68dc4600ebbd2a3d7b392c9e820766
chanwoo <chocw0402@gmail.com>
2022-08-31 18:28:25 +0900

    [feat] wal publisher

--- src/main/java/server/wal/app/notification/activemq/publisher/WalPublisher.java
+++ src/main/java/server/wal/app/notification/activemq/publisher/WalPublisher.java
@@ -1,11 +1,26 @@
 package server.wal.app.notification.activemq.publisher;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.activemq.ScheduledMessage;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+import server.wal.app.notification.activemq.constant.ActiveMQConstants;
+import server.wal.app.notification.activemq.publisher.dto.request.PubReservationDto;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-public class RabbitPublisher {
+public class WalPublisher {
 
+    private final JmsTemplate jmsTemplate;
+
+    public void publishReservation(PubReservationDto requestDto) {
+        jmsTemplate.convertAndSend(ActiveMQConstants.RESERVATION_QUEUE, requestDto.getUserId(), message -> {
+            message.setLongProperty(ScheduledMessage.AMQ_SCHEDULED_DELAY, requestDto.getDelay());
+            log.info(String.format("유저 (%s)의 예약 딜레이 시간은 (%s) 입니다", requestDto.getUserId(), message.getLongProperty(ScheduledMessage.AMQ_SCHEDULED_DELAY)));
+            return message;
+        });
+    }
 
 }
