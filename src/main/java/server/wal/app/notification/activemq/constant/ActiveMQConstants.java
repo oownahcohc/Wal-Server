6b8da61b4a44d83d419fa5f32758e702af1f53a2
chanwoo <chocw0402@gmail.com>
2022-08-31 18:27:56 +0900

    [feat] activemq queue name constants class

---
+++ src/main/java/server/wal/app/notification/activemq/constant/ActiveMQConstants.java
@@ -0,0 +1,11 @@
+package server.wal.app.notification.activemq.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ActiveMQConstants {
+
+    public static final String RESERVATION_QUEUE = "reservation.queue";
+
+}
