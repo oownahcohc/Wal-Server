616e04fefef86fd2fbefb404a9202e8718e5ade6
chanwoo <chocw0402@gmail.com>
2022-09-09 17:39:41 +0900

    [add] 해당 WalTimeType 의 시간정보 가져오는 메서드

--- src/main/java/server/wal/domain/common/enumerate/WalTimeType.java
+++ src/main/java/server/wal/domain/common/enumerate/WalTimeType.java
@@ -3,13 +3,14 @@ package server.wal.domain.common.enumerate;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import server.wal.common.model.EnumModel;
 import server.wal.common.util.TimeUtils;
 
 import java.time.LocalDateTime;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-public enum WalTimeType {
+public enum WalTimeType implements EnumModel {
 
     MORNING("아침", TimeUtils.getMorning()),
     AFTERNOON("점심", TimeUtils.getAfternoon()),
@@ -19,5 +20,19 @@ public enum WalTimeType {
 
     private final String value;
     private final LocalDateTime sendDueDate;
+
+    @Override
+    public String getKey() {
+        return name();
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    public LocalDateTime getTimeValue() {
+        return sendDueDate;
+    }
 
 }
