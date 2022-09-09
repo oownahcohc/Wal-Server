5339e7c001bc95f072e3f149600a872be0b65183
chanwoo <chocw0402@gmail.com>
2022-09-09 17:35:55 +0900

    [add] 해당 카테고리에 해당하는 nextwal 가져오기

--- src/main/java/server/wal/domain/nextWal/repository/NextWalRepositoryCustom.java
+++ src/main/java/server/wal/domain/nextWal/repository/NextWalRepositoryCustom.java
@@ -1,9 +1,12 @@
 package server.wal.domain.nextWal.repository;
 
+import server.wal.domain.common.enumerate.WalCategoryType;
 import server.wal.domain.nextWal.NextWal;
 
 import java.util.List;
+import java.util.Set;
 
 public interface NextWalRepositoryCustom {
     List<NextWal> findByUserId(Long userId);
+    List<NextWal> findByCategoryTypesAndUserId(Set<WalCategoryType> categoryTypes, Long userId);
 }
--- src/main/java/server/wal/domain/nextWal/repository/NextWalRepositoryCustomImpl.java
+++ src/main/java/server/wal/domain/nextWal/repository/NextWalRepositoryCustomImpl.java
@@ -2,9 +2,11 @@ package server.wal.domain.nextWal.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import server.wal.domain.common.enumerate.WalCategoryType;
 import server.wal.domain.nextWal.NextWal;
 
 import java.util.List;
+import java.util.Set;
 
 import static server.wal.domain.nextWal.QNextWal.*;
 
@@ -19,6 +21,16 @@ public class NextWalRepositoryCustomImpl implements NextWalRepositoryCustom {
                 .selectFrom(nextWal)
                 .where(nextWal.userId.eq(userId))
                 .fetch();
+    }
+
+    @Override
+    public List<NextWal> findByCategoryTypesAndUserId(Set<WalCategoryType> categoryTypes, Long userId) {
+        return query
+                .selectFrom(nextWal)
+                .where(
+                        nextWal.categoryType.in(categoryTypes),
+                        nextWal.userId.eq(userId)
+                ).fetch();
     }
 
 }
